b303e5c0be0172928a6ac0c899eefd78e5c68a7b
TSelmerworld <85435261+TSelmerworld@users.noreply.github.com>
2024-01-06 16:18:43 +1100

    Java Basic Week 1 Homework
    Java Basic Week 1 Homework to convert Fahrenheit to degree Celsius

---
+++ Main.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int F;
+		Scanner in = new Scanner(System.in);
+		F = in.nextInt();
+		System.out.println((int)((F-32)*(5.0/9.0)));
+
+	}
+
+}
